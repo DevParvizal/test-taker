53109723c125069b550b5de4c9385a2d251c5c7a
DevParvizal <142283113+DevParvizal@users.noreply.github.com>
2025-04-17 01:04:22 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+const API_CONFIG = {
+    baseUrl: 'https://test-taker-chi.vercel.app',
+    endpoints: {
+        generate: '/generate',
+        check: '/check'
+    }
+};
+
+export default API_CONFIG;
